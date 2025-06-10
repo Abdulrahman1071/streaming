9046f667914d15e983621003c9e72958c73fe130
Abdulrahman1071 <Dhomee37@gmail.com>
2025-06-10 15:14:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client({ checkUpdate: false });
+const express = require('express');
 
+const client = new Client({ checkUpdate: false });
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Express server للحفاظ على البوت شغال
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// Discord selfbot
 client.on('ready', async () => {
   console.log(`${client.user.username} is Ready For Working 24/7!`);
   client.user.setActivity('Sleeping', {
