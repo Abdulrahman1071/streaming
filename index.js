a0624f3b49021e51424dd0041c2088cd73c08b22
Abdulrahman1071 <Dhomee37@gmail.com>
2026-01-29 15:15:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const { Client } = require('discord.js-selfbot-v13');
-const express = require('express');
-
-const app = express();
 const client = new Client({ checkUpdate: false });
 
-// ===== Express Server =====
-app.get('/', (req, res) => {
-  res.send('Bot is running 🚀');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🌐 Web server running on port ${PORT}`);
-});
-
-// ===== Discord Selfbot =====
 client.on('ready', async () => {
   console.log(`${client.user.username} is Ready For Working 24/7!`);
-
-  client.user.setActivity('x20?', {
+  client.user.setActivity('جدو سيف استنى شويه', {
     type: 'STREAMING',
-    url: 'https://twitch.tv/x20'
+    url: 'https://twitch.tv/dl1' 
   });
 });
 
